1b2b5fe5ce1d3d26151ef406c2e28e1ab75657e2
Miguel Silva <mzsilvaw@gmail.com>
2026-03-18 08:49:23 -0300

    Update button-download.tsx

--- src/components/button-download/button-download.tsx
+++ src/components/button-download/button-download.tsx
@@ -3,7 +3,7 @@ import GitHubIco from "@/assets/svgs/githubIco";
 export default function ButtonDownload() {
   return (
     <a
-      href="https://github.com/miguelzackk"
+      href="https://github.com/miguelzack"
       target="_blank"
       rel="noopener noreferrer"
       className="flex py-[15px] px-[25px] justify-center items-center gap-2.5 rounded-[35px] bg-primary text-white text-xl font-normal hover:bg-orange-700 transition-all duration-300 ease-out hover:shadow-sm hover:-translate-y-1"
